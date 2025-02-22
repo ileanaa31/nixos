39c92a43e3551a73ed1be0ebcfa0a2370c12f7fa
Anton Pogrebnjak <contact@pantonius.dev>
2025-02-22 14:05:08 +0100

    Fixed proposition

--- flake.nix
+++ flake.nix
@@ -1,24 +1,26 @@
 {
-	description = "The Flake to rule them all";
+  description = "The Flake to rule them all";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
 
-	outputs = { nixpkgs, ... }@inputs: {
-		nixosConfigurations.robert = nixpkgs.lib.nixosSystem {
-			specialArgs = { inherit inputs; };
-			system = "x86_64-linux";
-			modules = [
-				./hosts/robert/configuration.nix
-			];
-		};
-		nixosConfigurations.shelby = nixpkgs.lib.nixosSystem {
-			specialArgs = { inherit inputs; };
-			system = "x86_64-linux";
-			modules = [
-				./hosts/shelby/configuration.nix
-			];
-		};
-	};
+  outputs =
+    { nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations.robert = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/robert/configuration.nix
+        ];
+      };
+      nixosConfigurations.shelby = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/shelby/configuration.nix
+        ];
+      };
+    };
 }
