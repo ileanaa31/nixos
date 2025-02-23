a18a69bc43d7071f3943ccc5d99c76617a58f860
Anton Pogrebnjak <contact@pantonius.dev>
2025-02-23 15:04:29 +0100

    Playing around with multi-host config

--- hosts/generic/configuration.nix
+++ hosts/generic/configuration.nix
@@ -1,61 +1,11 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.initrd.luks.devices."luks-efae76f9-a9ee-4ffc-a776-d4c2f487b803".device = "/dev/disk/by-uuid/efae76f9-a9ee-4ffc-a776-d4c2f487b803";
-  
-  # Graphics
-  hardware.graphics.enable = true;
-  boot.initrd.kernelModules = [ "amdgpu" ];
-  services.xserver.videoDrivers = [ "amdgpu" ];
-
-  # Enable sound with pipewire
-  # sound.enable = true;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
-  # Printing
-  services.printing.enable = true;
-  services.avahi = { # auto-discovery
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-
-  # Scanner
-  hardware.sane = {
-    enable = true;
-  };
-
-  # USB Stuff
-  boot.supportedFilesystems = [ "ntfs" "fat32" "exfat" ];
-  services.gvfs.enable = true;
-  services.udisks2.enable = true;
-
-  # Networking
-  networking.networkmanager.enable = true;
-
-  # Docker
-  virtualisation.docker.enable = true;
-
-  # annoying stuff with nix
-  programs.nix-ld.enable = true;
-
-  # Bluetooth
-  services.blueman.enable = true;
-  hardware.bluetooth.enable = true;
-
   # Flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
@@ -81,24 +31,17 @@
     variant = "altgr-intl";
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.anton = {
-    isNormalUser = true;
-    description = "Anton";
-    extraGroups = [ "networkmanager" "wheel" "docker" "scanner" "lp" ];
-  };
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
-   vim
-   wget
-   unzip
-   git
-   neofetch
-   home-manager
+    vim
+    wget
+    unzip
+    git
+    neofetch
+    home-manager
   ];
 
   # Enable the OpenSSH daemon.
@@ -111,5 +54,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
-
 }
--- hosts/robert/configuration.nix
+++ hosts/robert/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -7,5 +7,75 @@
     ./hyprland.nix
   ];
 
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices."luks-efae76f9-a9ee-4ffc-a776-d4c2f487b803".device = "/dev/disk/by-uuid/efae76f9-a9ee-4ffc-a776-d4c2f487b803";
+
+  # Graphics
+  hardware.graphics.enable = true;
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  # Enable sound with pipewire
+  # sound.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # Printing
+  services.printing.enable = true;
+  services.avahi = {
+    # auto-discovery
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+  # Scanner
+  hardware.sane = {
+    enable = true;
+  };
+
+  # USB Stuff
+  boot.supportedFilesystems = [
+    "ntfs"
+    "fat32"
+    "exfat"
+  ];
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
+
+  # Networking
+  networking.networkmanager.enable = true;
   networking.hostName = "robert"; # Define your hostname.
+
+  # Docker
+  virtualisation.docker.enable = true;
+
+  # annoying stuff with nix
+  programs.nix-ld.enable = true;
+
+  # Bluetooth
+  services.blueman.enable = true;
+  hardware.bluetooth.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.anton = {
+    isNormalUser = true;
+    description = "Anton";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
+      "scanner"
+      "lp"
+    ];
+  };
 }
